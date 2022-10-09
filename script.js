e1cf120e736aa5a42e0fd5a4ef935da4407c0030
AbuMudey <114142036+AbuMudey@users.noreply.github.com>
2022-10-09 20:33:55 +0200

    ändring i script

--- script.js
+++ script.js
@@ -1,54 +1,81 @@
+"use strict";   // info- https://www.w3schools.com/js/js_strict.asp
 
+
+// HTML-references
 const h1 = document.querySelector("#h1");
 const form = document.querySelector("#form");
-const username = document.querySelector("#username");
-const password = document.querySelector("#password");
-const login = document.querySelector("#login");
-const fel = document.querySelector("#fel");
 const logout = document.querySelector("#logout");
+const login = document.querySelector("#login");     //  - coupled with events/event listeners, otherwise "login" not needed here
+// const username = document.querySelector("#username")
+// const password = document.querySelector("#password")
+// const fel = document.querySelector("#fel");       
 
 
 
-// Three username and passwords
+// Array - Three username and passwords
 let objPeople = [
-
     {
-        username: "fredrik",
-        password: "12345",
-    },
-
-    {
-        username:"abukar",
-        password: "abc123",
+        usernameArray: "fredrik",   // used declarative property-names ('username' occurred multiple times)
+        passwordArray: "12345"  
     },
     {
-        username:"anna",
-        password:"anna1",
+        usernameArray: "abukar",
+        passwordArray: "abukar1"
     },
+    {
+        usernameArray: "anna",
+        passwordArray: "anna1"
+    }
 ]
 
- function getInfo() {
-   let username = document.getElementById("username").value
-   let password = document.getElementById("password").value
-   document.getElementById("h1").innerText ="Välkommen"
-   document.getElementById("form").style.display = "none"
-   logout.style.display ="block"
-
-// loop
-    for(i = 0; i < objPeople.length; i++) {
-          if(username == objPeople[i].username && password == objPeople[i].
-        password){
-            console.log(username + " is logged in !!!")
-        } else {
-            renderFalse();
-
-        }
-    };    
- };
-
- // felmeddelande om användar eller lösen är felaktigt
- function renderFalse(){
-    h1.innerText="fel användarnamn eller lösenord"
- }
 
 
+// Function - search and match
+function getInfo(e) {
+    
+    e.preventDefault();     // info - https://developer.mozilla.org/en-US/docs/Web/API/Event/preventDefault
+
+    const usernameEntry = document.getElementById("username").value;
+    const passwordEntry = document.getElementById("password").value;
+    document.getElementById("form").style.display = "none"
+    
+
+
+    // Explanation: loop array with find > create parameter 'person' to represent each iteration > if entered username/pw matches a user's username/pw from array, run if-block, otherwise run else-block)
+    let matchedPerson = objPeople.find(person => usernameEntry == person.usernameArray && passwordEntry == person.passwordArray);
+
+    if(matchedPerson){
+        // once logged in, write code to store user-data to localstorage - https://www.w3schools.com/jsrEF/prop_win_localstorage.asp
+        // JSON.parse(localStorage.setItem()) .....
+        // now, the user will stay logged in even if page is refreshed
+        // when logging out (log-out button), add click-event to button and link a function to delete the localstorage-data
+
+        // add "Welcome"-message/page here
+        h1.innerText = `${usernameEntry} is logged in - welcome home`
+        logout.style.display ="block"
+    } 
+    else(
+        renderFalse()
+    )
+};
+
+
+
+// Function - felmeddelande om användar eller lösen är felaktigt
+function renderFalse(){
+    h1.innerText="Fel användarnamn och/eller lösenord"
+}
+
+
+
+// Event listener - button-click
+login.addEventListener('click', getInfo);   // - eventlistener for "login"-button
+
+//function for logout
+function out(){
+    document.getElementById("form").style.display ="block"
+    logout.style.display ="none"
+    h1.innerText="Välkommen - vänligen logga in ovan"
+}
+// Event listener for - button-click
+logout.addEventListener('click', out);  // Eventlistener för "logout"-button
